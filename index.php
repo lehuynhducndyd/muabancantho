b329c47a005718ca8c20e844c919e508d7dd58a7
lehuynhducndyd <lehuynhducndyd@gmail.com>
2025-02-24 21:03:41 +0700

    khoi tao

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello PHP</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit consequatur blanditiis ab, nulla possimus error consequuntur beatae omnis perferendis fuga rerum obcaecati labore cum, quidem nisi amet libero quis eligendi. Laudantium, at, magnam voluptatibus fugit quas ut blanditiis doloremque voluptatem iste facilis fuga nam maiores neque, sint natus eum corrupti?</p>
+</body>
+</html>
